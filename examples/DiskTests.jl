fd6da3ce5888444689a691540cc5c8f16e91ca7d
Marin Lauber <48692086+marinlauber@users.noreply.github.com>
2024-05-08 08:58:43 +0200

    Update DiskTests.jl

--- examples/DiskTests.jl
+++ examples/DiskTests.jl
@@ -1,17 +1,5 @@
 using WaterLily,StaticArrays,CUDA,BiotSavartBCs
 
-function make_sim(; N=128, R=32, a=0.5, U=1, Re=1e3, mem=Array)
-    disk(x,t) = (r = √(x[2]^2+x[3]^2); √(x[1]^2+(r-min(r,R))^2)-1.5)
-    s(t) = ifelse(t<U/a,0.5a*t^2,U*(t-0.5U/a)) # displacement
-    move(x,t) = x - SA[R*s(t/R)-R,0,0] .- N/2  # move the center
-    Simulation((N,N,N), (0,0,0), R; U,ν=U*R/Re, body=AutoBody(disk,move), mem)
-end
-function make_sim_square(; N=128, R=32, a=0.5, U=1, Re=1e3, mem=Array)
-    square(x,t) = (y = x.-SA[0,clamp(x[2],-R,R),clamp(x[3],-R,R)]; √sum(abs2,y)-1.5)
-    s(t) = ifelse(t<U/a,0.5a*t^2,U*(t-0.5U/a)) # displacement
-    move(x,t) = x - SA[R*s(t/R)-R,0,0] .- N/2  # move the center
-    Simulation((N,N,N), (0,0,0), R; U,ν=U*R/Re, body=AutoBody(square,move), mem)
-end
 function make_sim_acc(; N=128, R=32, a=0.5, U=1, Re=1e3, mem=Array)
     disk(x,t) = (y=x-SA[-R,0,0].-N/2; r=√(y[2]^2+y[3]^2); √(y[1]^2+(r-min(r,R))^2)-1.5)
     Ut(i,t::T) where T = i==1 ? convert(T,a*t/R+(1.0+tanh(31.4*(t/R-1.0/a)))/2.0*(1-a*t/R)) : zero(T) # velocity BC
@@ -19,7 +7,6 @@ function make_sim_acc(; N=128, R=32, a=0.5, U=1, Re=1e3, mem=Array)
 end
 
 include("TwoD_plots.jl")
-include("Diagnostics.jl")
 using JLD2
 CIs = CartesianIndices
 N = 2^8; R = N/3
@@ -64,21 +51,6 @@ for use_biotsavart in [true]
         end
     end
 end
-
-
-# # read results for post processing
-a_star = 0.5; Ca = 1.0/3.0
-biot = jldopen("disk_$(N)D_$(R)R_$(true).jld2","r")
-# ref = jldopen("disk_$(N)D_$(R)R_$(false).jld2","r")
-forces_biot = reduce(vcat,biot["case"]["forces"]')
-# forces_ref = reduce(vcat,ref["case"]["forces"]')
-plot(forces_biot[:,1],forces_biot[:,2],label="Biot-Savart")
-# plot!(forces_ref[:,1],forces_ref[:,2]*(Ca*a_star),label="Reflection")
-
-# tₐ = 2.0
-# H(x::AbstractFloat) = ifelse(x < 0, zero(x), ifelse(x > 0, one(x), oftype(x,0.5)))
-# Dadiff(t,tₐ=0) = Ca * a_star*(√ν)*(√t - H(t-tₐ)√H(t-tₐ))
-
 # # plot!([0,3],[Ca,Ca],label=:none,ls=:dot,color=:black)
 # xlims!(0,3); ylims!(0,4)
 # xlabel!("Convective time"); ylabel!("( F/ρU²R² )/ Ca")
