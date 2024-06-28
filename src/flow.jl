afe0e2630651989c617c4976a108badb528cdbcb
marinlauber <marinlauber@tudelft.nl>
2024-06-28 15:03:23 +0200

    enables logging the pressure

--- src/flow.jl
+++ src/flow.jl
@@ -1,25 +1,28 @@
 # momentum step using bio_project
 import WaterLily: scale_u!,conv_diff!,BDIM!,CFL,accelerate!,time
+using LoggingExtras: @debug
 function biot_mom_step!(a::Flow{N},b,ω) where N
     a.u⁰ .= a.u; scale_u!(a,0)
     # predictor u → u'
+    @debug "mlp"
     U = BCTuple(a.U,@view(a.Δt[1:end-1]),N)
     conv_diff!(a.f,a.u⁰,a.σ,ν=a.ν);
     accelerate!(a.f,@view(a.Δt[1:end-1]),a.g,a.U)
     BDIM!(a);
     biot_project!(a,b,ω,U) # new
     # corrector u → u¹
+    @debug "mlc"
     U = BCTuple(a.U,a.Δt,N)
     conv_diff!(a.f,a.u,a.σ,ν=a.ν)
     accelerate!(a.f,a.Δt,a.g,a.U)
     BDIM!(a); scale_u!(a,0.5)
-    biot_project!(a,b,ω,U,w=0.5) # new
+    biot_project!(a,b,ω,U,0.5) # new
     push!(a.Δt,CFL(a))
 end
 
 # project using biot BCs
 import WaterLily: residual!,Vcycle!,smooth!,L∞,restrictML,BCTuple
-function biot_project!(a::Flow{n},ml_b::MultiLevelPoisson,ω,U;w=1,log=false,tol=1e-6,itmx=32) where n    
+function biot_project!(a::Flow{n},ml_b::MultiLevelPoisson,ω,U,w=1;tol=1e-6,itmx=32) where n    
     dt = w*a.Δt[end]; a.p .*= dt  # Scale p *= w*Δt
     fill_ω!(ω,a.u,a.μ₀,a.p)       # Compute ω=∇×(u-μ₀∇p)
     biotBC!(a.u,U,ω)            # Apply domain BCs
@@ -29,6 +32,7 @@ function biot_project!(a::Flow{n},ml_b::MultiLevelPoisson,ω,U;w=1,log=false,tol
     residual!(b); fix_resid!(b.r) # Set r=Ax-σ, and ensure sum(r)=0
 
     r₂ = L₂(b); nᵖ = 0; x₀ = point(ω)
+    @debug "ml, $nᵖ, $(L∞(b)), $r₂\n"
     while nᵖ<itmx
         x₀ .= b.x                 # Remember current solution
         Vcycle!(ml_b); smooth!(b) # Improve solution
@@ -36,12 +40,10 @@ function biot_project!(a::Flow{n},ml_b::MultiLevelPoisson,ω,U;w=1,log=false,tol
         fill_ω!(ω,a.μ₀,b.ϵ)       # vort update: Δω = -∇×μ₀∇ϵ
         update_resid!(b.r,a.u,b.z,ω) # Update domain BC and resid
         r₂ = L₂(b); nᵖ+=1
-        log && @show nᵖ,r₂
+        @debug "ml, $nᵖ, $(L∞(b)), $r₂\n"
         r₂<tol && break
     end
     push!(ml_b.n,nᵖ)
-    # (nᵖ<2 && length(ml_b.levels)>5) && pop!(ml_b.levels); # remove coarsest level if this was easy
-    # (nᵖ>4 && divisible(ml_b.levels[end])) && push!(ml_b.levels,restrictML(ml_b.levels[end])) # add a level if this was hard
     
     for i ∈ 1:n   # Project u -= μ₀∇p
         @loop a.u[I,i] -= b.L[I,i]*∂(i,I,b.x) over I ∈ inside(b.x)
