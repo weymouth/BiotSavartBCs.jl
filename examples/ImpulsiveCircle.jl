f360ecaf2e8e2765b6efb4acfc4b1cf4b97d7774
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2025-03-04 14:48:16 +0100

    Update figure again

--- examples/ImpulsiveCircle.jl
+++ examples/ImpulsiveCircle.jl
@@ -1,6 +1,6 @@
 using WaterLily,StaticArrays,CUDA,BiotSavartBCs
-function circ(D,m;Re=550,U=1,makeSim=BiotSimulation,kwargs...)
-    body = AutoBody((x,t)->hypot(x[1]-m÷2,x[2]-m÷2)-D÷2)
+function circ(D,m;Re=550,U=1,shift=0,makeSim=BiotSimulation,kwargs...)
+    body = AutoBody((x,t)->hypot(x[1]-m÷2,x[2]-m÷2-shift)-D÷2)
     makeSim((2m,m), (U,0), D; body, ν=U*D/Re, kwargs...)
 end
 
@@ -15,20 +15,7 @@ with_drag(data) = map(2:length(data)-1) do i
     (t=data.t[i],Ix=data.Ix[i],Cd=-2*(data.Ix[i+1]-data.Ix[i-1])/(data.t[i+1]-data.t[i-1]))
 end |> Table
 
-koumoutsakos = hcat([[0.043110735418427915, 1.4994160125588691],
-                [0.06339814032121693, 1.213695930443183],
-                [0.1344040574809806, 0.8673899287525665],
-                [0.5029585798816567, 0.7552846274604519],
-                [0.8613693998309375, 1.0203250815118947],
-                [1.256973795435333, 1.269354908827436],
-                [1.730346576500422, 1.272648230889989],
-                [2.2104818258664394, 1.175368192247313],
-                [2.656804733727809, 1.108954957130781],
-                [3.1470836855452236, 1.053957734573119],
-                [3.6238377007607756, 1.0138215191402002],
-                [4.097210481825865, 0.9816862697741817],
-                [4.567202028740489, 0.9586948436179206]]...)
-
+# Published data
 Gillis = hcat([[0.04311073541842814, 1.3588445839874406],
           [0.06339814032121693, 1.1211245018717544],
           [0.11411665257819092, 0.9245385822968241],
@@ -114,27 +101,38 @@ function small_time(t;Re=550, k=4√(t/Re))
     return π/√(Re*t)*(t₁ + t₂ + t₃ + t₄)
 end
 
+# Generate present method's data
+D,m_2,m_1 = 128,(4,5,6),(3,5,8,24)
+@time biot = map(m_2) do m
+    sim = circ(D,m*D÷2,shift=D÷8,mem=CUDA.CuArray)
+    e = m==4 ? 5 : 6
+    [update_Ix!(sim,t₀) for t₀ in 0:0.02:e] |>Table |> with_drag
+end;
+@time refl = map(m_1) do m
+    sim = circ(D,m*D,mem=CUDA.CuArray,makeSim=Simulation)
+    [update_Ix!(sim,t₀) for t₀ in 0:0.02:6] |>Table |> with_drag
+end;
+
+# Plot
 begin
-    # scatter(Billuard[1,1:5:end],Billuard[2,1:5:end],label="Billuard et al. ")
     scatter(koumoutsakos[1,:],koumoutsakos[2,:],label="Koumoutsakos and Leonard",m=(5, :square, :white, stroke(1, color)))
     scatter!(Gillis[1,:],Gillis[2,:],label="Gillis et al.",m=(5, :white, stroke(1, color)),c=:black,marker=:circle)
     plot!(collect(0:0.01:0.35),small_time.(0:0.01:0.35),ls=:dash,c=:black,label="Theoretical curve")
     bmap = palette(:Blues,5)
-    for (i,m) in enumerate((9,11,13))
-        sim = circ(128,m*128÷4,mem=CUDA.CuArray)
-        biot = [update_Ix!(sim,t₀) for t₀ in 0:0.02:6] |>Table |> with_drag
-        plot!(biot.t,biot.Cd,c=bmap[i+1],label="Present, D/W=$(round(4/m,digits=2))")
+    for (m,dat) in zip(m_2,biot)
+        m2 = m/2
+        mod(m2,1)==0 && (m2 = Int(m2))
+        plot!(dat.t,dat.Cd,c=bmap[m-2],label="Present, D/W=1/$m2")
     end
     rmap = palette(:Reds,6)
-    for (i,m) in enumerate((3,5,8,24))
-        sim = circ(128,m*128,mem=CUDA.CuArray,makeSim=Simulation)
-        refl = [update_Ix!(sim,t₀) for t₀ in 0:0.02:6] |>Table |> with_drag
-        plot!(refl.t,refl.Cd,c=rmap[i+1],ls=:dashdot,label="Reflection, D/W=1/$m")
+    for (i,m,dat) in zip(1:4,m_1,refl)
+        plot!(dat.t,dat.Cd,c=rmap[i+1],ls=:dashdot,label="Reflection, D/W=1/$m")
     end
-end; plot!(dpi=300,ylabel="Cd",xlabel="tU/D",ylims=(0,1.6),legend=:bottomright)
+end; plot!(dpi=300,ylabel="Drag coefficient",xlabel="convective time",ylims=(0,1.6),legend=:bottomright)
 savefig("ImpCircle_Cd.png")
 
-sim = circ(128,2*128,mem=CUDA.CuArray);sim_step!(sim,4,remeasure=false);
+# Flow plot
+sim = circ(D,2*D,mem=CUDA.CuArray);sim_step!(sim,4,remeasure=false);
 ω = sim.flow.σ
 @inside ω[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
 contourf(clamp.(ω[inside(ω)],-6,6)';aspect_ratio=:equal,c=:RdBu_11,clims=(-6,6),lw=0,levels=10)
