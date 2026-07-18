d170288682534c7ad5c69ea3054a91b39d3204e4
marinlauber <marinlauber@gmail.com>
2026-07-18 15:59:43 +0200

    add local files

--- examples/SpanwiseCylinder.jl
+++ examples/SpanwiseCylinder.jl
@@ -1,10 +1,10 @@
 using WaterLily, StaticArrays, BiotSavartBCs, WriteVTK
 
 # simulation constructor
-function spanwise_cylinder(; D=32, Lz=D÷4, Re=3_700, U=1, T=Float32, mem=Array)
+function spanwise_cylinder(;dims=(4,2,1), D=32, Re=1_000, U=1, T=Float32, mem=Array)
     body = AutoBody((x,t) -> √sum(abs2,SA[x[1]-D,x[2]-D]) - D/3.f0)
-    # default nimage=2
-    BiotSimulation((4D,2D,Lz), (U,0,0), D; ν=U*D/Re, body, T, mem, perdir=(3,), nimages=2)
+    length(dims)==3 && return BiotSimulation(dims.*D, (U,0,0), D; ν=U*D/Re, body, T, mem, perdir=(3,), nimages=1)
+    BiotSimulation(dims.*D, (U,0), D; ν=U*D/Re, body, T, mem)
 end
 
 # function for vtk writing
@@ -15,15 +15,25 @@ vtk_pressure(a::Simulation) = a.flow.p  |> Array
 # write vtk fields
 attrib = Dict("d"=>vtk_sdf, "u"=>vtk_velocity, "μ₀"=>vtk_mu0, "p"=>vtk_pressure)
 
-# make sim and writer
+# make 2D and 3D sim
 using CUDA
-sim = spanwise_cylinder(;D=128,mem=CuArray)
+sim2D = spanwise_cylinder(;dims=(4,2),   D=128, mem=CuArray)
+sim3D = spanwise_cylinder(;dims=(4,2,3), D=128, mem=CuArray, Re=12_000)
+
+# spread after a few steps
+println("Running 2D sim for 100 steps...")
+sim_step!(sim2D, 100.0; remeasure=false)
+println("Done, spreading to 3D...")
+WaterLily.spread!(sim3D, sim2D; dim=3, ϵ=0.0)
+
+# writer for the 3D sim
 writer = vtkWriter("SpanwiseCylinder"; attrib)
 
-# run
+# run the 3D sim
+println("Running 3D sim for 30 time units...")
 for t in range(0, 30.0; step=0.05)
-    sim_step!(sim, t; remeasure=false)
-    save!(writer, sim)
-    @show t
+    sim_step!(sim3D, t; remeasure=false)
+    sim_info(sim3D)
+    t≥25.0 && save!(writer, sim3D)
 end
 close(writer)
